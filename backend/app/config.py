f5ef5c86a0377450a235ea3c89ff9e91b119f6db
tamiminaser <tamimi.naser@yahoo.com>
2024-09-27 23:56:05 -0500

    updating config for database

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://scott:tiger@localhost/foo')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:1234@localhost/lextuit')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
